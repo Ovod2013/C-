c4afd336a358cea19b263651ffdbaa47d94ae47c
Ovod2013 <Ovod2013@yandex.ru>
2023-04-21 16:19:47 +0300

    повторил задачи на семинаре

---
+++ task5/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача №5 на входе одно число N, на выходе все целые числа в промежутке от -N до N
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+int num = - number;
+while (num <= number)
+{
+    Console.WriteLine(num + " ");
+
+    num=num+1;
+}
