73205bedc72c18b6c8ee862946d4813753b654ff
Ovod2013 <Ovod2013@yandex.ru>
2023-04-27 13:12:31 +0300

    добавил ! чтобы жёлтым не подчёркивал

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Написать программу, которая выдаёт квадрат вводимого числа
 // Пример: 4 -> 16
 Console.Write("Введите число: "); // Запрос числа
-int number = int.Parse(Console.ReadLine());// Пользователь вводит число
+int number = int.Parse(Console.ReadLine() !);// Пользователь вводит число
 // Console.WriteLine(number * number); // Первый вариант
 int sdr = number * number;// Второй вариант, кладём результат сначала в переменную
 Console.WriteLine("Квадрат числа = " + sdr);// Вывод результата
